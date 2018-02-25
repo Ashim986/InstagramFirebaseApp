7c36ab5b4722403008862017c2472b778b84a31a
ashim986 <ashim44@gmail.com>
2018-02-25 14:04:15 -0600

    - Setup login screen EOF 1 - Added asset catalogue content for all images - layout textfield for username, passowrd , and email created

--- InstagramFirebaseApplication/ViewController.swift
+++ InstagramFirebaseApplication/ViewController.swift
@@ -11,9 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     let plusPhotoVeiw : UIButton = {
-       let button = UIButton(type: .system)
-//        button.setImage(<#T##image: UIImage?##UIImage?#>, for: <#T##UIControlState#>)
-        button.backgroundColor = .yellow
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.backgroundColor = .clear
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -21,25 +21,68 @@ class ViewController: UIViewController {
     let emailTextField : UITextField = {
         let textField = UITextField()
         textField.placeholder = "Email"
-        textField.backgroundColor = .lightGray
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
         textField.borderStyle = .roundedRect
+        textField.font = UIFont.systemFont(ofSize: 14)
         textField.translatesAutoresizingMaskIntoConstraints = false
        return textField
     }()
     
+    let usernameTextField : UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Username"
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.borderStyle = .roundedRect
+        textField.font = UIFont.systemFont(ofSize: 14)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let passwordTextField : UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.borderStyle = .roundedRect
+        textField.isSecureTextEntry = true
+        textField.font = UIFont.systemFont(ofSize: 14)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let signUpButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupInputField()
+    }
+    
+    
+    fileprivate func setupInputField(){
         
         view.addSubview(plusPhotoVeiw)
-        view.addSubview(emailTextField)
-        
-        viewConstraint()
-        
-    }
-    private func viewConstraint(){
-      
         NSLayoutConstraint.activate([plusPhotoVeiw.topAnchor.constraint(equalTo: view.topAnchor, constant: 40), plusPhotoVeiw.centerXAnchor.constraint(equalTo: view.centerXAnchor),plusPhotoVeiw.widthAnchor.constraint(equalToConstant: 140), plusPhotoVeiw.heightAnchor.constraint(equalToConstant: 140)])
-        NSLayoutConstraint.activate([emailTextField.topAnchor.constraint(equalTo: plusPhotoVeiw.bottomAnchor, constant: 10), emailTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40), emailTextField.rightAnchor.constraint(equalTo: view.leftAnchor, constant: -40), emailTextField.heightAnchor.constraint(equalToConstant: 50)])
+        
+        // Stack View
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextField,usernameTextField,passwordTextField, signUpButton])
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([stackView.topAnchor.constraint(equalTo: plusPhotoVeiw.bottomAnchor, constant: 20), stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40), stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40), stackView.heightAnchor.constraint(equalToConstant: 200)])
+     
     }
 
       
